d089940b259a0196b85f661ea328fca972f62d68
RosasSebastian2003 <rosassebastian2003@gmail.com>
2024-01-10 05:07:17 -0600

    before tailwind

--- Oracle/Oracle/urls.py
+++ Oracle/Oracle/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Core import views as core
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core.index, name='index' )
 ]
