f75db27bd6907e37ca1001c652fcc2fb24f8a510
RosasSebastian2003 <rosassebastian2003@gmail.com>
2024-02-12 11:05:53 -0600

    tailwind added as a dependency

--- Oracle/Oracle/settings.py
+++ Oracle/Oracle/settings.py
@@ -37,10 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'Core',
     'team.apps.TeamConfig',
-    'season'
+    'season',
+    
+    
+    'tailwind',
+    'theme', 
+    'django_browser_reload'
 ]
+
+# Tailwind settings
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'Oracle.urls'
